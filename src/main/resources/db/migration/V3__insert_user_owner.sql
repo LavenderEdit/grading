6647e76944d308e8845b56e39c2903fe831534f6
LavenderEdit <gercermagden@gmail.com>
2025-10-05 17:12:40 -0500

    feat: agregar migración para insertar usuario y rol por defecto
    - Se ha añadido una migración SQL para insertar un usuario con nombre 'Joan' y correo electrónico 'joan_owner@gmail.com' en la tabla `users`, solo si no existe un usuario con ese nombre.
    - También se ha añadido una migración para asignar el rol con `role_id = 1` al usuario insertado (usuario con `user_id = 1`) en la tabla `user_rol`, solo si esa asignación no existe.
    
    Estas migraciones aseguran que el usuario 'Joan' y su rol por defecto estén presentes en la base de datos, sin duplicar entradas.

--- src/main/resources/db/migration/V3__insert_user_owner.sql
+++ src/main/resources/db/migration/V3__insert_user_owner.sql
@@ -3,3 +3,10 @@
  * Created: Oct 2, 2025
  */
 
+insert into `users` (name, email)
+select 'Joan', 'joan_owner@gmail.com'
+where not exists (select 1 from users where name = 'Joan');
+
+insert into `user_rol` (user_id, role_id)
+select 1, 1
+where not exists (select 1 from user_rol where user_id = 1);
