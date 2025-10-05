fb278e4a0205a8c2875e173b3a83ea857e4df260
LavenderEdit <gercermagden@gmail.com>
2025-10-05 17:12:57 -0500

    feat: agregar migración para insertar usuario y rol de administrador
    - Se ha añadido una migración SQL para insertar un usuario con nombre 'Vidal' y correo electrónico 'vidal_admin@gmail.com' en la tabla `users`, solo si no existe un usuario con ese nombre.
    - También se ha añadido una migración para asignar el rol con `role_id = 2` (rol de administrador) al usuario insertado (usuario con `user_id = 2`) en la tabla `user_rol`, solo si esa asignación no existe.
    
    Estas migraciones aseguran que el usuario 'Vidal' y su rol de administrador estén presentes en la base de datos, sin duplicar entradas.

--- src/main/resources/db/migration/V4__insert_user_admin.sql
+++ src/main/resources/db/migration/V4__insert_user_admin.sql
@@ -2,3 +2,11 @@
  * Author:  Studios TKOH!
  * Created: Oct 2, 2025
  */
+
+insert into `users` (name, email)
+select 'Vidal', 'vidal_admin@gmail.com'
+where not exists (select 1 from users where name = 'Vidal');
+
+insert into `user_rol` (user_id, role_id)
+select 2, 2
+where not exists (select 1 from user_rol where user_id = 2);
