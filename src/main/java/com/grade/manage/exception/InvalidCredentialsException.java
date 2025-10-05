c94c40ddb0d891cd5b9b9eadc657a8187e4df151
LavenderEdit <gercermagden@gmail.com>
2025-10-05 17:09:53 -0500

    feat: agregar excepción personalizada InvalidCredentialsException
    - Se ha creado la excepción personalizada `InvalidCredentialsException` que extiende `RuntimeException`.
    - Esta excepción se utilizará para manejar casos donde las credenciales de acceso sean inválidas, como cuando un usuario ingresa una contraseña incorrecta.
    
    Esta clase proporciona un manejo de errores más específico y claro cuando ocurren fallos relacionados con la autenticación de usuarios.

---
+++ src/main/java/com/grade/manage/exception/InvalidCredentialsException.java
@@ -0,0 +1,12 @@
+package com.grade.manage.exception;
+
+/**
+ *
+ * @author Studios TKOH!
+ */
+public class InvalidCredentialsException extends RuntimeException {
+
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
