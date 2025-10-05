4fa3766884cda78970eabbfd58f564d4b0c46344
LavenderEdit <gercermagden@gmail.com>
2025-10-05 17:10:07 -0500

    feat: agregar excepción personalizada RoleNotFoundException
    - Se ha creado la excepción personalizada `RoleNotFoundException` que extiende `RuntimeException`.
    - Esta excepción se utilizará para manejar casos donde no se encuentre un rol específico en el sistema, lo que puede ocurrir al intentar asignar o consultar roles inexistentes.
    
    Esta clase mejora el manejo de errores relacionados con la gestión de roles de usuario, proporcionando un mensaje claro cuando un rol no es encontrado.

---
+++ src/main/java/com/grade/manage/exception/RoleNotFoundException.java
@@ -0,0 +1,12 @@
+package com.grade.manage.exception;
+
+/**
+ *
+ * @author Studios TKOH!
+ */
+public class RoleNotFoundException extends RuntimeException {
+
+    public RoleNotFoundException(String message) {
+        super(message);
+    }
+}
