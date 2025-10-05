c275064e8f399e406bd68eaad529ec615581ff3a
LavenderEdit <gercermagden@gmail.com>
2025-10-05 17:11:08 -0500

    feat: crear interfaz genérica DtoCrudService para operaciones CRUD
    - Se ha implementado la interfaz `DtoCrudService`, que extiende `DtoWriteService` y `DtoReadService` para proporcionar operaciones genéricas de lectura y escritura en el servicio.
    - La interfaz está parametrizada con los tipos `D1`, `D2`, `D3` (DTOs) e `ID` (tipo de ID), permitiendo su reutilización en distintos módulos del proyecto.
      
    Esta interfaz proporciona una estructura común para los servicios que gestionan las operaciones CRUD de manera flexible y reutilizable, integrando tanto la lectura como la escritura de datos.

---
+++ src/main/java/com/grade/manage/service/generic/DtoCrudService.java
@@ -0,0 +1,14 @@
+package com.grade.manage.service.generic;
+
+/**
+ *
+ * @author Studios TKOH!
+ * @param <D1> DTO 1 -> The one that returns the data
+ * @param <D2> DTO 2 -> The one that is used to create
+ * @param <D3> DTO 3 -> The one that is used to update (in some special cases it
+ * may be used only the one that returns the data if they are very similar)
+ * @param <ID> ID -> The type of ID from the model/class
+ */
+public interface DtoCrudService<D1, D2, D3, ID> extends DtoWriteService<D1, D2, D3, ID>, DtoReadService<D1, ID> {
+
+}
