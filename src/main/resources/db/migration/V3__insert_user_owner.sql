dd4c36915bb82eb6929121df42e4e3b0cadb70ed
LavenderEdit <gercermagden@gmail.com>
2025-10-06 08:55:23 -0500

    feat(migrations): Añadida la migración en sql para crear un usuario dueño

--- src/main/resources/db/migration/V3__insert_user_owner.sql
+++ src/main/resources/db/migration/V3__insert_user_owner.sql
@@ -3,10 +3,18 @@
  * Created: Oct 2, 2025
  */
 
-insert into `users` (name, email)
-select 'Joan', 'joan_owner@gmail.com'
-where not exists (select 1 from users where name = 'Joan');
+INSERT INTO users (name, email, password, current_score, enabled, locked, password_changed_at, created_by, created_at, modified_by, modified_at)
+SELECT 'Joan', 'joan_owner@gmail.com', '$2a$12$YUj5ZCyMuO3GWD3uptZpIunTb8As1Yqt2xR3UNu1E6Ur.gyOtfbIW', 0, 1, 0,
+       UTC_TIMESTAMP(), 'system', UTC_TIMESTAMP(), 'system', UTC_TIMESTAMP()
+WHERE NOT EXISTS (SELECT 1 FROM users WHERE email = 'joan_owner@gmail.com');
 
-insert into `user_rol` (user_id, role_id)
-select 1, 1
-where not exists (select 1 from user_rol where user_id = 1);
+INSERT INTO user_rol (user_id, role_id)
+SELECT u.id, r.id
+FROM users u
+JOIN roles r ON r.name = 'Owner'
+WHERE u.email = 'joan_owner@gmail.com'
+  AND NOT EXISTS (
+      SELECT 1
+      FROM user_rol ur
+      WHERE ur.user_id = u.id
+        AND ur.role_id = r.id);
