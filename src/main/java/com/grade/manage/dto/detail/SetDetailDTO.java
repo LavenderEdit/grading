10ee44cf5110e4473b69d26c4dfd2490baae7f33
LavenderEdit <gercermagden@gmail.com>
2025-10-05 22:45:07 -0500

    refactor: rename fields in SetDetailDTO for clarity and consistency
    - Renamed `tempassigns` to `assignments` for clearer representation of the assignments related to the set.
    - Renamed `countTempAssigns` to `assignmentCount` to better reflect the field's purpose of holding the count of assignments.
    - These changes improve the readability and maintain consistency across related DTOs.

--- src/main/java/com/grade/manage/dto/detail/SetDetailDTO.java
+++ src/main/java/com/grade/manage/dto/detail/SetDetailDTO.java
@@ -14,6 +14,6 @@ public class SetDetailDTO {
     private Long id;
     private String name;
     private String description;
-    private List<TempAssignDTO> tempassigns;
-    private Integer countTempAssigns;
+    private List<TempAssignDTO> assignments;
+    private Integer assignmentCount;
 }
