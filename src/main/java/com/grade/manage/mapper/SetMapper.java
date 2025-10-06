0f0aa2f25c84dba3093eaa99babc7896f3c1975f
LavenderEdit <gercermagden@gmail.com>
2025-10-05 22:57:13 -0500

    feat: add SetMapper for converting ActivitySet to DTOs
    - Implemented `SetMapper` to map `ActivitySet` model to `SetDTO` and `SetDetailDTO`.
    - Added support for counting assignments in `SetDTO` using the `assignmentCount` field.
    - Refined the `toDetail` method to include `assignments` from `relations`.
    - Enabled usage of `TempAssignMapper` for handling assignment-related mapping.

--- src/main/java/com/grade/manage/mapper/SetMapper.java
+++ src/main/java/com/grade/manage/mapper/SetMapper.java
@@ -1,12 +1,26 @@
 package com.grade.manage.mapper;
 
+import com.grade.manage.dto.detail.SetDetailDTO;
+import com.grade.manage.dto.simple.SetDTO;
+import com.grade.manage.model.ActivitySet;
+import java.util.Collection;
+import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
  *
  * @author Studios TKOH!
  */
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = TempAssignMapper.class)
 public interface SetMapper {
-    // Waiting to be implemented
+
+    @Mapping(target = "assignmentCount", expression = "java(set.getRelations().size())")
+    SetDTO toDto(ActivitySet set);
+
+    List<SetDTO> toDtoList(Collection<ActivitySet> sets);
+
+    @Mapping(target = "assignments", source = "relations")
+    @Mapping(target = "assignmentCount", expression = "java(set.getRelations().size())")
+    SetDetailDTO toDetail(ActivitySet set);
 }
