062c7ae4e76d63dd6cb145ecb4b065a635429f2f
LavenderEdit <gercermagden@gmail.com>
2025-10-05 22:54:30 -0500

    feat: implement RoleMapper for DTO and entity conversions
    - Added mappings for `RoleSummary`, `RoleDetailDTO`, and `UserSummary` conversions from `Role` and `User` models.
    - Implemented methods to convert `RoleCreateDTO` to `Role` and to update `Role` entities using `RoleUpdateDTO`.
    - Included `NullValuePropertyMappingStrategy.IGNORE` to handle null properties during `Role` entity updates.
    - Improved collection mappings with `toSummaryList` for converting a collection of `Role` objects to `RoleSummary` DTOs.

--- src/main/java/com/grade/manage/mapper/RoleMapper.java
+++ src/main/java/com/grade/manage/mapper/RoleMapper.java
@@ -1,6 +1,19 @@
 package com.grade.manage.mapper;
 
+import com.grade.manage.dto.create.RoleCreateDTO;
+import com.grade.manage.dto.detail.RoleDetailDTO;
+import com.grade.manage.dto.summary.RoleSummary;
+import com.grade.manage.dto.summary.UserSummary;
+import com.grade.manage.dto.update.RoleUpdateDTO;
+import com.grade.manage.model.Role;
+import com.grade.manage.model.User;
+import java.util.Collection;
+import java.util.List;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 /**
  *
@@ -8,5 +21,21 @@ import org.mapstruct.Mapper;
  */
 @Mapper(componentModel = "spring")
 public interface RoleMapper {
-    // Waiting to be implemented
+
+    RoleSummary toSummary(Role role);
+
+    List<RoleSummary> toSummaryList(Collection<Role> roles);
+
+    @Mapping(target = "users", source = "users")
+    RoleDetailDTO toDetail(Role role);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "users", ignore = true)
+    Role toEntity(RoleCreateDTO dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "users", ignore = true)
+    void updateEntity(RoleUpdateDTO dto, @MappingTarget Role role);
+
+    UserSummary toUserSummary(User user);
 }
